4dbfb93ada384fed9698472cb450189f6bbf64fb
Gabriel Bernardo <gabriel.bernardo@estudante.ufla.br>
2020-07-21 12:00:19 -0300

    Primeiras telas de navegção criadas

---
+++ lib/main.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:sneakers_apps/detailsPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePagesState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      title: new Text(
+        'Welcome In SplashScreen',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+      seconds: 2,
+      navigateAfterSeconds: _MyHomePageState(),
+      image: new Image.asset("imgs/loading.gif"),
+      backgroundColor: Colors.black,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 150.0,
+      onClick: () => print("Flutter Egypt"),
+      loaderColor: Colors.white,
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF21BFBD),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 15.0, left: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                Container(
+                    width: 125.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.filter_list),
+                          color: Colors.white,
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          color: Colors.white,
+                          onPressed: () {},
+                        )
+                      ],
+                    ))
+              ],
+            ),
+          ),
+          SizedBox(height: 25.0),
+          Padding(
+            padding: EdgeInsets.only(left: 40.0),
+            child: Row(
+              children: <Widget>[
+                Text('Calendar',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0)),
+                SizedBox(width: 10.0),
+                Text('Sneaker',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                        fontSize: 25.0))
+              ],
+            ),
+          ),
+          SizedBox(height: 40.0),
+          Container(
+            height: MediaQuery.of(context).size.height - 185.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+            ),
+            child: ListView(
+              primary: false,
+              padding: EdgeInsets.only(left: 25.0, right: 20.0),
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(top: 45.0),
+                    child: Container(
+                        height: MediaQuery.of(context).size.height - 400.0,
+                        child: ListView(children: [
+                          _buildsneakers(
+                              'imgs/j1.jpg', 'Jordan one chicago', '\$1000.00'),
+                          _buildsneakers('imgs/j1_blue.png',
+                              'Jorda one off white', '\$1000.00'),
+                          _buildsneakers('imgs/j4.png', 'Jordan 4', '\$100.00'),
+                          _buildsneakers('imgs/y1.png', 'Yeezy', '\$24.00')
+                        ]))),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      height: 65.0,
+                      width: 60.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.grey,
+                            style: BorderStyle.solid,
+                            width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.search, color: Colors.black),
+                      ),
+                    ),
+                    Container(
+                      height: 65.0,
+                      width: 60.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.grey,
+                            style: BorderStyle.solid,
+                            width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Center(
+                        child: Icon(Icons.shopping_basket, color: Colors.black),
+                      ),
+                    ),
+                    Container(
+                      height: 65.0,
+                      width: 120.0,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                              width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Color(0xFF1C1428)),
+                      child: Center(
+                          child: Text('Checkout',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.white,
+                                  fontSize: 15.0))),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildsneakers(String imgPath, String sneakName, String price) {
+    return Padding(
+        padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+        child: InkWell(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => DetailsPage(
+                      heroTag: imgPath,
+                      sneakName: sneakName,
+                      foodPrice: price)));
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                    child: Row(children: [
+                  Hero(
+                      tag: imgPath,
+                      child: Image(
+                          image: AssetImage(imgPath),
+                          fit: BoxFit.scaleDown,
+                          height: 75.0,
+                          width: 75.0)),
+                  SizedBox(width: 20.0),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(sneakName,
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold)),
+                        Text(price,
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 15.0,
+                                color: Colors.grey))
+                      ])
+                ])),
+                IconButton(
+                    icon: Icon(Icons.add),
+                    color: Colors.black,
+                    onPressed: () {})
+              ],
+            )));
+  }
+}
